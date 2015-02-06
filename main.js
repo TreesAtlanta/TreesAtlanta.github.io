46d4a0bca35debdfa9aed69f2fb69dbbfd812d9a
nikhil-kulkarni <nikhil896@gmail.com>
2015-02-06 14:54:11 -0500

    Firebase stuff

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 //BASE DEPENDENCIES
 var fb = new Firebase("https://treesatlanta.firebaseio.com/");
 
-$('#submit').click(function (e) {
+$('#passwordInput').keypress(function (e) {
+  if(e.keyCode == 13) {
+  	var name = $('#nameInput').val()
+    var username = $('#emailInput').val();
+    var password = $('#passwordInput').val();
     fb.createUser({
-        'email' : $('#emailInput').val(),
-        'password' : $('#passwordInput').val()
+      name: name,
+      email: username,
+      password: password
     }, function(error) {
-        if (error) {
-            fw.alert(error.message, 'Error');
-        } else {
-            print("Success");
+      if (error) {
+        switch (error.code) {
+          case "EMAIL_TAKEN":
+            console.log('Email already taken');
+            break;
+          case "INVALID_EMAIL":
+            console.log('Invalid email');
+            break;
+          default:
+            console.log('Something went wrong', error);
         }
+      } else {
+        console.log('User account created!');
+      }
     });
+    $('#nameInput').val('');
+    $('#usernameInput').val('');
+    $('#passwordInput').val('');
+  }
 });
