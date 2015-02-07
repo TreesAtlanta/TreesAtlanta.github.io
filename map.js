e46915dfdae7c033fdabe4ae34c8ebceee09a529
nikhil-kulkarni <nikhil896@gmail.com>
2015-02-06 23:35:39 -0500

    Authenication and Database creation

--- map.js
+++ map.js
@@ -1,4 +1,5 @@
 // var $$ = DOM7;
+var fb = new Firebase("https://treesatlproject.firebaseio.com/");
 
 function initialize() {
         var mapOptions = {
@@ -8,6 +9,32 @@ function initialize() {
         var map = new google.maps.Map(document.getElementById('map-canvas'),
                                       mapOptions);
 
+              fb.push({
+                cLatitude: "33.5230",
+                cLongitude: "-84.3700",
+                treeStatus: "Maintanence needed",
+                comments: "Needs watering",
+                dates: {
+                  mulchDate: "01/01/01",
+                  pruneDate: "03/12/07",
+                  fertilizedDate: "12/14/12",
+                  insectideDate: "09/12/13"
+                }
+             });
+
+              fb.push({
+                cLatitude: "33.5190",
+                cLongitude: "-84.3700",
+                treeStatus: "Good",
+                comments: "Needs mulch",
+                dates: {
+                  mulchDate: "01/01/10",
+                  pruneDate: "03/12/23",
+                  fertilizedDate: "12/14/90",
+                  insectideDate: "09/12/89"
+                }
+             });
+
         setMarkers(map, trees);
       }
 
@@ -16,6 +43,7 @@ function initialize() {
        * the order in which these markers should display on top of each
        * other.
        */
+
       var trees = [
         ['Tree 1', 33.7580, -84.3700, "description1"],
         ['Tree 2', 33.750, -84.3910, "description2"],
@@ -31,6 +59,15 @@ function initialize() {
         var contentString;
 
         for (var i = 0; i < locations.length; i++) {
+          var temp = locations[i];
+          var x;
+          fb.child("status").on("value", function(snapshot) {
+            x = snapshot.val();
+          });
+          temp[3] = x;
+        }
+
+        for (var i = 0; i < locations.length; i++) {
           var tree = locations[i];
           var myLatLng = new google.maps.LatLng(tree[1], tree[2]);
           
@@ -43,7 +80,9 @@ function initialize() {
 
           contentString = tree[3];
 
-          google.maps.event.addListener(marker, 'click', getInfoCallback(map, contentString));
+          google.maps.event.addListener(marker, 'click', function() {
+            window.location = "profile.html";
+          });
 
         }
       }
@@ -51,7 +90,7 @@ function initialize() {
       function getInfoCallback(map, content) {
           var infowindow = new google.maps.InfoWindow({content: content});
           return function() {
-            infowindow.setContent(content); 
+            infowindow.setContent(content);
             infowindow.open(map, this);
         };
       }
